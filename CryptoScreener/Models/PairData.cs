e2c8b9526c813bfea8b28b2cc521405e8a40e1ca
MD. ATIKUR RAHMAN <golenur02@gmail.com>
2025-06-07 23:32:21 +0600

    1

---
+++ CryptoScreener/Models/PairData.cs
@@ -0,0 +1,11 @@
+﻿namespace CryptoScreener.Models
+{
+    public class PairData
+    {
+        public string Symbol { get; set; }
+        public decimal Price { get; set; }
+        public decimal PriceChange { get; set; }
+        public decimal RSI { get; set; }
+        public string RSIStatus { get; set; }
+    }
+}
